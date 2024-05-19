2b1be671b54bbb45fe46764787ee136188af6718
ITCSIU22006 <153516825+ITCSIU22006@users.noreply.github.com>
2024-05-19 23:13:40 +0700

    Delete src directory

--- src/SchoolDatabase.java
+++
@@ -1,7 +0,0 @@
-public class SchoolDatabase {
-
-    public void updateStudentRecord(String studentName, String offenseDescription) {
-        // Logic to update the student record in the database (implementation not shown in the diagram)
-        System.out.println("Updating student record for: " + studentName);
-    }
-}
